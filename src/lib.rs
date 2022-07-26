1e0b3e5a3c0bdb569bd03bb8f429cfc77f503ace
Thomas Holloway <nyxtom@gmail.com>
2022-07-26 13:22:21 -0500

    Add server new example

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
 mod node;
 mod router;
+mod server;
 
 pub use node::*;
 pub use router::*;
+pub use server::*;
+
+/// Creates a new server to process requests on a protocol.
+///
+/// ## Examples
+/// ```ignore
+/// fn index() {}
+/// fn authorize() {}
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let mut app = sidemount::new();
+///     app.mount(authorize);
+///     app.at("/foo").get(index);
+///
+///     app.listen("127.0.0.1:7000").await
+/// }
+/// ```
+pub fn new() -> Server {
+    Server {}
+}
 
 /// Creates a new [Router] implementation using the default
 /// radix tree node router with support for mounting middleware.
