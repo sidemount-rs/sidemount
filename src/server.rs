49c7aedb2fe697ded67010367c36f2b3ae50e014
Thomas Holloway <nyxtom@gmail.com>
2022-07-26 14:43:13 -0500

    Setup server with default router

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,25 @@
-pub struct Server {}
+use crate::{Handler, Route, Router};
 
-impl Server {}
+pub struct Server {
+    router: Router,
+}
+
+impl Server {
+    /// Creates a new server and default router.
+    pub fn new() -> Self {
+        Server {
+            router: Router::new(),
+        }
+    }
+
+    /// Creates a new node node or returns a mutable reference to an existing one.
+    pub fn at(&mut self, path: &str) -> &mut Route {
+        self.router.at(path)
+    }
+
+    /// Mounts a handler implementation as middleware to be optionally executed with
+    /// each of the routes once a route has been found.
+    pub fn mount(&mut self, handler: impl Handler) {
+        self.router.mount(handler);
+    }
+}
